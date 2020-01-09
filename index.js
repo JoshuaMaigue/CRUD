f953a06ecdb5341c4e6b3684fbd6c1b0cdc0f04c
Joshua Maigue <Joshmaigue01@gmail.com>
2020-01-09 16:47:57 +0800

    Added get function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 const express = require('express');
 var app = express();
 
+//Set up connection
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -9,9 +10,25 @@ var mysqlConnection = mysql.createConnection({
     database: 'inventory'
 });
 
+//Check connection
 mysqlConnection.connect((err) => {
     if (!err)
         console.log('DB connection succeeded');
     else
         console.log('DB connection failed \n Error : ' + JSON.stringify(err,undefined, 2));
 });
+
+//Setup port
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
+
+//Get all items
+app.get('/items', (req,res) => {
+    mysqlConnection.query('SELECT * FROM inventory.items', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
